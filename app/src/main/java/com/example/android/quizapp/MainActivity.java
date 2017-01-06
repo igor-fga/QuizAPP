7c9da0b1dda67baf812fbf77fc5fd3b3628d4b70
Igor Andrade <Igor Andrade>
2017-01-05 23:49:15 -0200

    Inserido checkbox e edittext

--- app/src/main/java/com/example/android/quizapp/MainActivity.java
+++ app/src/main/java/com/example/android/quizapp/MainActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.RadioGroup;
 import android.widget.Toast;
 
@@ -14,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
     private RadioGroup rg02;
     private RadioGroup rg03;
     private RadioGroup rg04;
-    private RadioGroup rg05;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,10 +31,9 @@ public class MainActivity extends AppCompatActivity {
         btnSubmit = (Button) findViewById(R.id.btnSubmit);
 
         rg01 = (RadioGroup) findViewById(R.id.rg01);
-        rg02 = (RadioGroup) findViewById(R.id.rg02);
-        rg03 = (RadioGroup) findViewById(R.id.rg03);
-        rg04 = (RadioGroup) findViewById(R.id.rg04);
-        rg05 = (RadioGroup) findViewById(R.id.rg05);
+        rg02 = (RadioGroup) findViewById(R.id.rg03);
+        rg03 = (RadioGroup) findViewById(R.id.rg04);
+        rg04 = (RadioGroup) findViewById(R.id.rg05);
 
         btnSubmit.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -44,22 +45,31 @@ public class MainActivity extends AppCompatActivity {
                 int selectedId02 = rg02.getCheckedRadioButtonId();
                 int selectedId03 = rg03.getCheckedRadioButtonId();
                 int selectedId04 = rg04.getCheckedRadioButtonId();
-                int selectedId05 = rg05.getCheckedRadioButtonId();
 
-                if (selectedId01 == -1 || selectedId02 == -1 || selectedId03 == -1 || selectedId04 == -1 || selectedId05 == -1) {
+                CheckBox chk01 = (CheckBox) findViewById(R.id.chkReturn);
+                CheckBox chk02 = (CheckBox) findViewById(R.id.chkAttack);
+                CheckBox chk03 = (CheckBox) findViewById(R.id.chkBeyond);
+
+                if ((selectedId01 == -1 || selectedId02 == -1 || selectedId03 == -1 || selectedId04 == -1) ||
+                        !chk01.isChecked() && !chk02.isChecked() && !chk03.isChecked()) {
                     Toast.makeText(MainActivity.this,
                             getString(R.string.AnswerAll), Toast.LENGTH_LONG).show();
                 } else {
 
 
                     result += (selectedId01 == 1) ? 1 : 0;
-                    result += (selectedId02 == 6) ? 1 : 0;
+                    result += (selectedId02 == 5) ? 1 : 0;
                     result += (selectedId03 == 8) ? 1 : 0;
-                    result += (selectedId04 == 11) ? 1 : 0;
-                    result += (selectedId05 == 13) ? 1 : 0;
+                    result += (selectedId04 == 10) ? 1 : 0;
+
+                    if (chk01.isChecked() && chk02.isChecked() && !chk03.isChecked())
+                        result += 1;
+
+                    EditText nameField = (EditText) findViewById(R.id.Name);
+                    String name = nameField.getText().toString();
 
                     Toast.makeText(MainActivity.this,
-                            getString(R.string.Got) + " " + result + " " + getString(R.string.Ans), Toast.LENGTH_LONG).show();
+                            name + " " + getString(R.string.Got) + " " + result + " " + getString(R.string.Ans), Toast.LENGTH_LONG).show();
 
                 }
 
